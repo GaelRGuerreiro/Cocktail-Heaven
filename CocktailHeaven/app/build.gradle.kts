2349eb8d7ce93488eb55dbfdf05198d5bede0b6e
GAEL RG <grodriguezg22@fpcoruna.afundacion.org>
2024-10-03 01:11:12 +0200

    Cambios menores de visualizacion

--- CocktailHeaven/app/build.gradle.kts
+++ CocktailHeaven/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.cocktailheaven"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.cocktailheaven"
